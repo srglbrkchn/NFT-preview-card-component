b10521b973a8ead8141d5f05c2b836df63af7d59
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2022-02-10 00:42:53 +0100

    Initial commit.

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Footer from "./Footer";
+
+const App = () => {
+    return (
+        <div>
+            Equilibrium #3429
+            Our Equilibrium collection promotes balance and calm.
+            0.041 ETH
+            3 days left
+            Creation of Jules Wyvern
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
